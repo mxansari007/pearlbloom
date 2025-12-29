56dc8ebad1b0552aae92db9d14c04c1aa40da772
maaz <mxansari007@gmail.com>
2025-12-29 23:04:52 +0530

    new buy logic

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -136,7 +136,6 @@ export default async function ProductPage({
 
       {/* Reviews Section */}
       <section className="product-page__reviews">
-        <h2 className="product-page__heading">Customer Reviews</h2>
         <Reviews productId={product.id} />
       </section>
 
